773eebcdec6088f6f934f395e1cf9c6446a86424
bari199 <bari1999smab@gmail.com>
2026-08-27 11:34:07 -0700

    fix auth context filename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+//added here
 import App from "./App.jsx";
 import { AuthProvider } from "./context/authContext.jsx";
 import "./index.css";
